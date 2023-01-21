33038acce47c3f3c3b9245b1509b636601fa9273
harshMaitreya <harshmaitreya6@gmail.com>
2023-01-21 21:08:13 +0530

    Day10

---
+++ Day10/Day10.js
@@ -0,0 +1,10 @@
+function string_reverse(str)
+{
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("VITbHOPAL"));
+console.log(string_reverse("CodeStreak"));
+console.log(string_reverse("JavaScript"));
+console.log(string_reverse("codingchallenge"));
+console.log(string_reverse("helloworld"));
